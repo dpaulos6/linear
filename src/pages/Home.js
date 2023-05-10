740fd3abcfc322eda90289b7fa2c5ac0c747ab49
dpaulos6 <itzframepvp@outlook.com>
2023-05-10 09:43:02 +0100

    Several Project Setup Changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,64 @@
-import React, { useEffect, useState } from 'react';
-import './Home.css';
+import React, { useEffect, useState, Fragment } from 'react'
+import { Dialog, Disclosure, Popover, Transition } from '@headlessui/react'
+import {
+  ArrowPathIcon,
+  Bars3Icon,
+  ChartPieIcon,
+  CursorArrowRaysIcon,
+  FingerPrintIcon,
+  SquaresPlusIcon,
+  XMarkIcon,
+} from '@heroicons/react/24/outline'
+import { ChevronDownIcon, PhoneIcon, PlayCircleIcon } from '@heroicons/react/20/solid'
+
+import './Home.scss';
 
 import { Octokit } from "@octokit/core";
 
 const { client_token } = require('../config.json');
 
+const products = [
+  { name: 'Analytics', description: 'Get a better understanding of your traffic', href: '#', icon: ChartPieIcon },
+  { name: 'Engagement', description: 'Speak directly to your customers', href: '#', icon: CursorArrowRaysIcon },
+  { name: 'Security', description: 'Your customers’ data will be safe and secure', href: '#', icon: FingerPrintIcon },
+  { name: 'Integrations', description: 'Connect with third-party tools', href: '#', icon: SquaresPlusIcon },
+  { name: 'Automations', description: 'Build strategic funnels that will convert', href: '#', icon: ArrowPathIcon },
+]
+const callsToAction = [
+  { name: 'Watch demo', href: '#', icon: PlayCircleIcon },
+  { name: 'Contact sales', href: '#', icon: PhoneIcon },
+]
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
+
+async function commitsShow(value){
+  if(value == null){return;}
+  const x = document.querySelector('.commits-list');
+
+  const text = value.full_name;
+  const arr = text.split('/');
+  const owner = arr[0];
+  const repo = arr[1];
+  
+  const octokit = new Octokit({ auth: `${client_token}` });
+  const response = await octokit
+    .request("GET /repos/{owner}/{repo}/commits",{
+      owner: owner, 
+      repo: repo
+    })
+    .catch(error => console.error(error));
+  
+  console.log(response);
+
+  return response.data;
+};
+
 export default function Home(){
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
   const [repos, setRepos] = useState([]);
+  const [commits, setCommits] = useState([]);
 
   useEffect(() => {
     const octokit = new Octokit({ auth: `${client_token}` });
@@ -15,17 +67,147 @@ export default function Home(){
         type: 'owner',
       })
       .then(response => {
+        console.log(response);
         setRepos(response.data);
       })
       .catch(error => console.error(error));
   }, []);
 
+
   return (
     <>
+      <header className="bg-white">
+        <nav className="mx-auto flex h-24 max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
+          <div className="flex lg:flex-1">
+            <a href="#" className="">
+              <span className="sr-only">Your Company</span>
+              <img className="h-8 w-auto mr-12" src="linearlogo_nobg.png" alt="" />
+            </a>
+            <div className="hidden items-center md:flex md:gap-x-6 lg:gap-x-12">
+              <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+                Pull Requests
+              </a>
+              <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+                Issues
+              </a>
+              <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+                Explore
+              </a>
+            </div>
+          </div>
+          <div className="flex lg:hidden">
+            <button
+              type="button"
+              className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+              onClick={() => setMobileMenuOpen(true)}
+            >
+              <span className="sr-only">Open main menu</span>
+              <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+            </button>
+          </div>
+          <div className="hidden min-w-[250px] h-full py-1 items-center lg:flex lg:gap-x-12">
+            <input className='linear-search bg-gray-200/75 rounded-md w-full h-full' type='search' placeholder='Search' />
+          </div>
+          <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+            <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+              Log in <span aria-hidden="true">&rarr;</span>
+            </a>
+          </div>
+        </nav>
+        <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
+          <div className="fixed inset-0 z-10" />
+          <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+            <div className="flex items-center justify-between">
+              <a href="#" className="-m-1.5 p-1.5">
+                <span className="sr-only">Your Company</span>
+                <img
+                  className="h-8 w-auto"
+                  src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                  alt=""
+                />
+              </a>
+              <button
+                type="button"
+                className="-m-2.5 rounded-md p-2.5 text-gray-700"
+                onClick={() => setMobileMenuOpen(false)}
+              >
+                <span className="sr-only">Close menu</span>
+                <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+              </button>
+            </div>
+            <div className="mt-6 flow-root">
+              <div className="-my-6 divide-y divide-gray-500/10">
+                <div className="space-y-2 py-6">
+                  <Disclosure as="div" className="-mx-3">
+                    {({ open }) => (
+                      <>
+                        <Disclosure.Button className="flex w-full items-center justify-between rounded-lg py-2 pl-3 pr-3.5 text-base font-semibold leading-7 hover:bg-gray-50">
+                          Product
+                          <ChevronDownIcon
+                            className={classNames(open ? 'rotate-180' : '', 'h-5 w-5 flex-none')}
+                            aria-hidden="true"
+                          />
+                        </Disclosure.Button>
+                        <Disclosure.Panel className="mt-2 space-y-2">
+                          {[...products, ...callsToAction].map((item) => (
+                            <Disclosure.Button
+                              key={item.name}
+                              as="a"
+                              href={item.href}
+                              className="block rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                            >
+                              {item.name}
+                            </Disclosure.Button>
+                          ))}
+                        </Disclosure.Panel>
+                      </>
+                    )}
+                  </Disclosure>
+                  <a
+                    href="#"
+                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                  >
+                    Features
+                  </a>
+                  <a
+                    href="#"
+                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                  >
+                    Marketplace
+                  </a>
+                  <a
+                    href="#"
+                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                  >
+                    Company
+                  </a>
+                </div>
+                <div className="py-6">
+                  <a
+                    href="#"
+                    className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                  >
+                    Log in
+                  </a>
+                </div>
+              </div>
+            </div>
+          </Dialog.Panel>
+        </Dialog>
+      </header>
+
       <div className="repos">
         {repos.map(item => (
-          <p key={item.id}>{item.name}</p>
+          <p key={item.id} onClick={async () => {
+            const commitsData = await commitsShow(item);
+            setCommits(commitsData);
+          }}>{item.name}</p>
         ))}
+      </div>
+      <div className="commits-list">
+        {/* {commits.map(c => (
+          <p key={c.id}>{c.name}</p>
+        ))} */}
       </div>
     </>
   );
